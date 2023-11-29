c45de857240a8613dd0b44bd0be1cae4d85ab084
Jesús <j2a0a0a5@gmail.com>
2023-11-28 21:38:28 -0400

    carga inicial

---
+++ java/Nose/src/lotesito/Lotesito.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lotesito;
+
+/**
+ *
+ * @author j2a0a
+ */
+public class Lotesito {
+    
+}
