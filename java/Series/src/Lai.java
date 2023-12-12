805e2b1f773bafb8fc5d3d0b82871217872c00b3
Jesús <j2a0a0a5@gmail.com>
2023-12-12 12:47:34 -0400

    cosas

--- java/Series/src/Lai.java
+++ java/Series/src/Lai.java
@@ -1,47 +1,25 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+import java.io.IOException;
 
 /**
  *
- * @author j2a0a
+ * @author Lai
  */
 public class Lai {
-    int cantidadBlanco = 0;
-    //de grados a radianes
-    public static double Radianes(double angulo) {
-        return(Math.toRadians(angulo));
+
+    public static void main(String[] args) throws IOException {
+        //System.out.println("ola");
+        int fila = Serie1.registroCantidad();
+        //System.out.println("ola");
+        String nonbreLote[]= new String[fila];
+        double matrizDatos [][] = new double[fila][6];
+        double matrizPromedio [][] =new double[fila][2];
+        String vectorStatus []= new String [fila];
+        
+        Serie1.llenado(matrizDatos, nonbreLote);
+        Serie1.salida1(matrizDatos, nonbreLote);
+        Serie1.calculos(matrizDatos, matrizPromedio);
+        Serie1.status(matrizPromedio, vectorStatus);
+        Serie1.salida2(nonbreLote, matrizPromedio, vectorStatus);
     }
-    //velocidad x
-    public static double VelocidadX(double velocidadInicial, double angRadianes){
-        return(velocidadInicial * Math.cos(angRadianes));
-    }
-    //velocidad y
-    public static double VelocidadY(double velocidadInicial, double angRadianes){
-        return(velocidadInicial * Math.sin(angRadianes));
-    }
-    //tiempo
-    public static double Tiempo(double distanciaBlanco, double velocidadX){
-        return(distanciaBlanco/velocidadX);
-    }
-    //altura
-    public static double Altura(double velocidadY, double tiempo){
-        return((velocidadY*tiempo)- (9.8*tiempo*tiempo/2));
-    }
-    //condicion de caida
-    public static String Condicion(double alturaProyectil){
-        int ALTURA_PARED = 20;
-        if(alturaProyectil <= ALTURA_PARED && alturaProyectil>0){
-        //++cantidadBlanco;
-        return("si pego en el blanco");
-        }
-        else if(alturaProyectil<=0){
-        return ("cayo antes");
-        }
-        else if(alturaProyectil>ALTURA_PARED){
-        return ("cayo despues");
-        }
-        return null;
-    }
+    
 }
--- java/Series/src/Serie1.java
+++ java/Series/src/Serie1.java
@@ -1,9 +1,75 @@
-/**
- *
- * @author j2a0a
- */
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Serie1 {
-    public static double Terminos(double x, double y, double n, int contador){
-        return (Math.pow(-1, contador) * ((Math.pow(x, contador) * Math.pow(y, n - contador + 1)) / (n - contador + 1) ));
+    //metodo1
+    public static int registroCantidad() throws IOException{
+        int contador =0;
+        Scanner archivo = new Scanner(new File("src\\Archivos\\Lotes.txt"));
+        while(archivo.hasNextLine()){
+            archivo.nextLine();
+            contador++;
+        }
+        archivo.close();
+        return contador;
+    }
+    //metodo 2
+    public static void llenado(double matrizDatos[][], String numeroLote []) throws IOException{
+        Scanner archivo = new Scanner(new File("src\\Archivos\\Lotes.txt"));
+        int f =0;
+        while(archivo.hasNextLine()){
+                numeroLote[f] = archivo.next();
+                for (int j = 0; j < matrizDatos[0].length; j++) {
+                    matrizDatos[f][j] = archivo.nextDouble();
+                }
+                f++;
+                //System.out.println("ola");
+        }
+        archivo.close();
+    }
+    //metodo 3
+    public static void salida1(double matrizDatos[][], String numeroLote []){
+        System.out.println("No Lote         --%Carbono--        --%Magnesio--");
+        for (int i = 0; i < numeroLote.length; i++) {
+            System.out.printf("%-7s",numeroLote[i]);
+            for (int j = 0; j < matrizDatos[0].length; j++) {
+                System.out.printf("%6.2f", matrizDatos[i][j]); 
+            }
+            System.out.println();  
+        }
+    }
+    //metodo 4
+    public static void calculos(double matrizDatos[][], double matrizPromedio [][]){
+        double sumaC,sumaM;
+        for (int i = 0; i < matrizPromedio.length; i++) {
+            sumaC =0;
+            sumaM =0;
+
+            sumaC = matrizDatos[i][0]+matrizDatos[i][1]+matrizDatos[i][2];
+            sumaM = matrizDatos[i][3]+matrizDatos[i][4]+matrizDatos[i][5];
+            matrizPromedio[i][0]=sumaC/3;
+            matrizPromedio[i][1]=sumaM/3;
+        }
+    }
+    //metodo 5
+    public static void status(double matrizPromedio [][], String vectorStatus []){
+        for (int i = 0; i < vectorStatus.length; i++) {
+            if ((24.92<matrizPromedio[i][0]) && (matrizPromedio[i][0]<27.92) && (matrizPromedio[i][1]>19.84) && (matrizPromedio[i][1]<25.3)) {
+                vectorStatus[i] = "Aprobado";
+            }   
+            else {vectorStatus[i] ="Rechazado";}
+        }
+    }
+    //metodo 6
+    public static void salida2(String vectorNombre [], double matrizPromedio [][], String vectorStatus[]){
+        System.out.println("Resultados de laboratorio");
+        System.out.println("Cmax: 27.92,    Cmin: 24.92");
+        System.out.println("MgMax: 25.30,  MgMin: 19.84");
+        System.out.println("no lote    Prom %C     Prom %Mg    Estatus");
+        
+        for (int i = 0; i < vectorStatus.length; i++) {
+            System.out.printf("%8s        %6.2f       %6.2f      %12s\n",vectorNombre[i],matrizPromedio[i][0],matrizPromedio[i][1],vectorStatus[i]);
+        }
     }
 }
